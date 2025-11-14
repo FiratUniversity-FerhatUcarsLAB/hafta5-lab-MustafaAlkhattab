ab3fe3d43f6e1ec00ea196e6c318d6573fb6c0ab
MustafaAlkhattab <250541607@firat.edu.tr>
2025-11-14 20:17:44 +0300

    Update FizikFormul.java

--- FizikFormul.java
+++ FizikFormul.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [Mustafa Alkhattab]
+ * Ogrenci No: [250541607]
+ * Tarih: [14/11/2025]
  * Aciklama: Gorev 2 - Fizik Formulu Asistani
  *
  * Bu program temel fizik formullerini kullanarak
